10031848098da4922376757b1d23136882a13118
i330688 <sean.saito@sap.com>
2018-10-21 14:06:38 +0800

    Added argparse

--- metrics_main.py
+++ metrics_main.py
@@ -3,13 +3,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import argparse
 import json
 import logging
+import os
 import sys
 
 from code_book_defenses.config import targeted_attacks, untargeted_attacks, checkpoint_mapping
 from code_book_defenses.config import train_params_cifar, target_types, train_params_mnist, \
     train_params_fmnist
+from code_book_defenses.constants import PATHS, DATASETS
 from code_book_defenses.data_providers.cifar import Cifar10DataProvider
 from code_book_defenses.data_providers.fmnist import FMNISTDataProvider
 from code_book_defenses.data_providers.mnist import MNISTDataProvider
@@ -22,8 +25,16 @@ if __name__ == '__main__':
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
     logger = logging.getLogger(__name__)
 
-    dataset = sys.argv[1]
-    gpu = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True,
+                        help='Name of the dataset (C10, MNIST, FMNIST)')
+    parser.add_argument('--gpus', type=str, required=True,
+                        help='GPUs to run on separated by commas (e.g. --gpu=0,1,2)')
+
+    args = parser.parse_args()
+    args = vars(args)
+    dataset = args['dataset']
+    gpus = args['gpus']
 
     logger.info('Gathering metrics')
     untargeted_attack_results = {}
@@ -35,11 +46,11 @@ if __name__ == '__main__':
         targeted_attack_results[attack] = {}
 
     target_type_to_checkpoint_dict = checkpoint_mapping[dataset]
-    if dataset == 'C10':
+    if dataset == DATASETS.C10:
         data_provider = Cifar10DataProvider(**train_params_cifar)
-    elif dataset == 'MNIST':
+    elif dataset == DATASETS.MNIST:
         data_provider = MNISTDataProvider(**train_params_mnist)
-    elif dataset == 'FMNIST':
+    elif dataset == DATASETS.FMNIST:
         data_provider = FMNISTDataProvider(**train_params_fmnist)
     else:
         logger.error('Invalid dataset')
@@ -56,7 +67,7 @@ if __name__ == '__main__':
                 targeted=False,
                 data_provider=data_provider,
                 batch_size=10,
-                gpus=gpu
+                gpus=gpus
             )
             untargeted_attack_results[attack][target_type] = temp_res
 
@@ -71,12 +82,16 @@ if __name__ == '__main__':
                 targeted=True,
                 data_provider=data_provider,
                 batch_size=10,
-                gpus=gpu
+                gpus=gpus
             )
             targeted_attack_results[attack][target_type] = temp_res
 
-    untargeted_results_path = 'results/{}_untargeted_results.json'.format(dataset)
-    targeted_results_path = 'results/{}_targeted_results.json'.format(dataset)
+    if not os.path.exists(PATHS.COMPARISON_RESULTS):
+        os.makedirs(PATHS.COMPARISON_RESULTS)
+
+    untargeted_results_path = os.path.join(PATHS.COMPARISON_RESULTS,
+                                           '{}_untargeted_results.json'.format(dataset))
+    targeted_results_path = os.path.join('{}_targeted_results.json'.format(dataset))
 
     with open(untargeted_results_path, 'w') as fp:
         json.dump(untargeted_attack_results, fp)
